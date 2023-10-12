1465a9a1a2918166c50ed279f283aad1eec243e1
matt-plank <matthew_plank@outlook.com>
2023-10-12 18:06:34 +0100

    Added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Articles from "./pages/articles";
+import Contact from "./pages/contact";
 import Home from "./pages/home";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/articles" exact element={<Articles />} />
+        <Route path="/contact" exact element={<Contact />} />
       </Routes>
     </Router>
   );
